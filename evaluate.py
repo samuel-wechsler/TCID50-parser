c553258d13ed1e99372d264a02372ab482aac1a4
Samuel Wechsler <samuel.wechsler.SW@gmail.com>
2023-03-09 16:24:47 +0100

    fix tcid50 bugs

--- evaluate.py
+++ evaluate.py
@@ -6,30 +6,54 @@ neural network. Based on the classification (infected / not-infected) the Tissue
 Dose-50% (TCID50) can be calculated.
 """
 
+import os
 
-def spear_karb(plate, d_0, d, c_0):
+def parse_plates(day, cell, data_dir):
+    coords = []
+
+    for filename in os.listdir(data_dir):
+        if (day in filename) and (cell in filename):
+            # parse column and row
+            row_col = filename.split('_')[2]
+            col = row_col[:1]
+            row = row_col[1:]
+            coords.append((row, col))
+    
+    return coords
+
+parse_plates("day3", "Hep2", "data/merge")
+
+def spear_karb(plate, d, d_0):
     """
     This function returns as TCDI50 value based on a plate that is represented as a matrix.
     Each column and row corresponds to the position of the cell culture well on the plate, the
     entry (either 0 or 1) reflects the infection state.
     d: log10 of dilution factor
     d_0: log10 of dilution of the first well
-    c_0: log10 of lowest dilution
     """
+    # special case: no fully infected rows
+    if not any([sum(row) == len(row) for row in plate]):
+        d_0 += 0.5
+
     # find fully infected row with greatest dilution
     row0 = find_x0(plate)
 
-    # assert that there is a fully inected row
-    if row0 is None:
-        raise Exception("There are no fully infected rows. The initial virus titer must have been to low.")
-    x_0 = c_0 - (row0) * d
+    # calculate the log10 concentration of the first fully infected row
+    x_0 = d_0 - (row0) * d
 
     # calculate sum of fractions of infected wells per row
     s = 0
-    for row in plate[row0:]:
+
+    # smooth out data
+    plate = sorted(plate, key=lambda row: (sum(row) / len(row)), reverse=True)
+    # remove duplicates
+    p = []
+    [p.append(row) for row in plate if row not in p]
+
+    for row in p:
         s += (sum(row) / len(row))
 
-    return 10 ** -((x_0 + d/2 - d * s) - d_0)
+    return 10 ** -((x_0 + d/2 - d * s) + d_0)
 
 
 def find_x0(plate):
@@ -38,20 +62,34 @@ def find_x0(plate):
     that's still fullyinfected and returns the index of that row 
     as an int.
     """
-    row0 = None
+    row0 = 0
     for row in range(len(plate)):
         if sum(plate[row]) == len(plate[row]):
             row0 = row
     return row0
         
+
+# plate = [
+#     [0, 0, 0, 0, 0, 0, 0, 0],
+#     [1, 1, 1, 1, 1, 1, 1, 0],
+#     [1, 1, 1, 1, 1, 1, 0, 0],
+#     [1, 1, 0, 0, 0, 0, 0, 0],
+#     [0, 0, 0, 0, 0, 0, 0, 0]
+# ]
+
 plate = [
-    [1, 1, 1, 1, 1, 1, 1, 1],
-    [1, 1, 1, 1, 1, 1, 0, 1],
-    [1, 1, 1, 1, 0, 1, 1, 0],
-    [0, 1, 0, 1, 1, 0, 1, 0],
-    [0, 0, 0, 1, 0, 0, 0, 0]
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 1],
+    [1, 1, 1, 0],
+    [1, 0, 0, 0],
+    [0, 0, 0, 0],
 ]
 
-res = spear_karb(plate, 1, 1, -1, )
+res = spear_karb(plate, 1, -1)
 
 print('%.2E' % res)
