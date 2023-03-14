9d1bd3bc0a7caefaa805695a1ab63becd68136bb
Samuel Wechsler <samuel.wechsler.SW@gmail.com>
2023-03-14 11:16:03 +0100

    build new data pipeline

--- data_eng.py
+++ data_eng.py
@@ -1,6 +1,7 @@
 import os
 import sys
-import shutil
+from skimage.io import imread, imsave
+from skimage import exposure
 
 from PIL import Image
 
@@ -15,7 +16,7 @@ def main():
         parse_files(input_dir, output_dir)
             
 
-def parse_files(input_dir, output_dir, filetype="png"):
+def parse_files_old(input_dir, output_dir, filetype="png"):
 
     for filename in os.listdir(input_dir):
         if filename in [".DS_Store"]:
@@ -33,6 +34,33 @@ def parse_files(input_dir, output_dir, filetype="png"):
         
         im = Image.open(src)
         im.save(dst)
+
+def parse_files(input_dir, output_dir, filetype="png"):
+
+    for dirpath, dirnames, filenames in os.walk(input_dir):
+        for filename in filenames:
+            if filename.endswith('.tif') and "GFP" in filename:
+                src = os.path.join(dirpath, filename)
+
+                paths = dirpath.split('/')
+                infos = [path.split('_') for path in paths]
+                meta = []
+                meta.extend(infos[2][-3:])
+                meta.extend(infos[3][-2:])
+
+                filename = "_".join(meta) + "_GFP"
+
+
+                dst = os.path.join(output_dir, filename + '.' + filetype)
+
+
+                print(f"converting {filename}")
+
+                image = imread(src)
+                image_dark = exposure.adjust_gamma(image, gamma=2,gain=1)
+                imsave(dst, image_dark)
+                
+                
 
 if __name__ == "__main__":
     main()
