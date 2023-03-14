4c4630967608d2320b300bb7232ef04083089d6c
Samuel Wechsler <samuel.wechsler.SW@gmail.com>
2023-03-14 11:16:39 +0100

    add rescale, convolution + maxpooling; remove one hidden layer

--- test_train.py
+++ test_train.py
@@ -6,17 +6,17 @@ import tensorflow as tf
 from sklearn.model_selection import train_test_split
 import cv2
 
-IMG_WIDTH = 256
-IMG_HEIGHT = 256
-TEST_SIZE = 0.4
+IMG_WIDTH = 128
+IMG_HEIGHT = 128
+TEST_SIZE = 0.2
 EPOCHS = 10
 
 def main():
     if len(sys.argv) not in [3, 4]:
-        sys.exit("Usage: python test_train.py data_directory classifications [model.h5]")
+        sys.exit("Usage: python test_train.py data_directory [model.h5]")
     
     # Get image arrays and labels for all images
-    images, labels = load_data(sys.argv[1], sys.argv[2])
+    images, labels = load_data(sys.argv[1])
 
     # Split data into training and testing sets
     labels = tf.keras.utils.to_categorical(labels)
@@ -35,12 +35,12 @@ def main():
     model.evaluate(x_test,  y_test, verbose=2)
 
     if len(sys.argv) == 4:
-        filename = sys.argv[3]
+        filename = sys.argv[2]
         model.save(filename)
         print(f"Model saved to {filename}.")
 
 
-def load_data(data_dir, classficiations):
+def load_data_old(data_dir, classficiations):
     """
     This function loads data from a data directory and their corresponding classficications
     from a given text file.
@@ -78,6 +78,38 @@ def load_data(data_dir, classficiations):
 
     return (images, labels)
 
+def load_data(data_dir):
+    """
+    This function loads data from a data directory and their corresponding classficications
+    from a given text file.
+    The function the returns both lists in a tuple.
+    """
+    skip = ['.DS_Store'] 
+    images = []
+    labels = []
+
+    for filename in os.listdir(data_dir):
+        print("parsing file ", filename)
+
+        path = os.path.join(data_dir, filename)
+
+        if not os.path.isfile(path) or filename in skip:
+            continue
+
+        # parse label
+        label = 0 if 'M' in filename else 1
+        labels.append(label)
+
+        # parse image as ndarray
+        im = cv2.imread(path)
+
+        # resize image
+        resizeIM = cv2.resize(im, (IMG_HEIGHT, IMG_WIDTH))
+        print(resizeIM.shape)
+        images.append(resizeIM)
+
+    return (images, labels)
+
 
 def get_model():
     """
@@ -89,10 +121,13 @@ def get_model():
     # Create a convolutional neural network
     model = tf.keras.models.Sequential(
         [
-            # Perform convolution and pooling twice
-            tf.keras.layers.Conv2D(
-                32, (3,3), activation="relu", input_shape=(IMG_HEIGHT,IMG_WIDTH,3)
-            ),
+            # Rescale pixel values
+            tf.keras.layers.Rescaling(1./255),
+
+            # Perform convolution and pooling three times
+            tf.keras.layers.Conv2D(32, (3,3), activation="relu", input_shape=(IMG_HEIGHT,IMG_WIDTH,3)),
+            tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
+            tf.keras.layers.Conv2D(32, (3,3), activation="relu"),
             tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
             tf.keras.layers.Conv2D(32, (3,3), activation="relu"),
             tf.keras.layers.MaxPooling2D(pool_size=(2,2)),
@@ -103,15 +138,11 @@ def get_model():
             # Add hidden layers with dropout
             tf.keras.layers.Dense(128, activation="relu"),
             tf.keras.layers.Dropout(0.5),
-            tf.keras.layers.Dense(128, activation="relu"),
-            tf.keras.layers.Dropout(0.5),
 
             # Add an output layer with output units for all 2 categories
             tf.keras.layers.Dense(2, activation="softmax")
         ]
     )
-    
-    print(model.summary())
 
     # Train neural net
     model.compile(
