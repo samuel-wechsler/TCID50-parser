46837ab43beac12d60df01287955d155e2b56fc7
Samuel Wechsler <samuel.wechsler.SW@gmail.com>
2023-03-19 09:22:42 +0100

    implement save_plate function

--- evaluate.py
+++ evaluate.py
@@ -11,7 +11,7 @@ import sys
 import numpy as np
 
 import matplotlib.pyplot as plt
-from PIL import Image
+from PIL import Image, ImageDraw, ImageFont
 
 import tensorflow as tf
 
@@ -79,10 +79,9 @@ def evaluate(dir, model, classnames=[1, 0]):
 
 def evaluate_plate(plate, model):
     """
-    This function takes in a matrix (i.e. a list of lists) representing
-    a plate of cell cultures, where each element is a file path to an image 
-    file, and returns a matrix where each element is either 1 (infected) 
-    or 0 (not infected). It accepts two arguments:
+    This function takes a matrix of paths to images as an argument,
+    evaluates each image with a specified model and returns the
+    predictions.
 
     plate: A matrix representing the plate of cell cultures.
     model: A TensorFlow Keras model to be used for prediction.
@@ -98,6 +97,117 @@ def evaluate_plate(plate, model):
         eval_plate.append(eval_row)
 
     return eval_plate
+
+
+def get_plates(data_dir, col_range, row_range):
+    """
+    This function returns a matrix containing file paths to image files 
+    from a directory containing file paths to image files. 
+    It accepts three arguments:
+
+    data_dir: A string representing the path to the directory containing the image files.
+    col_range: A tuple representing the range of columns on the plate.
+    row_range: A tuple representing the range of rows on the plate.
+    """
+    filenames = os.listdir(data_dir)
+    plate = []
+
+    row_range = [chr(i)
+                 for i in range(ord(row_range[0]), ord(row_range[1]) + 1)]
+    col_range = list(range(col_range[0], col_range[1] + 1))
+
+    # loop through all coordinates of a plate
+    for row_nb in row_range:
+        row = []
+
+        for col_nb in col_range:
+            coord = row_nb + str(col_nb)
+
+            # find matches
+            matches = [file for file in filenames if coord in file]
+            file_path = os.path.join(data_dir, matches[0])
+            row.append(file_path)
+
+        plate.append(row)
+
+    return plate
+
+
+def display_plate(plate):
+    """
+    This function creates a terminal representation of a well plate where
+    each infected well is marked with an X. It accepts
+    one argument:
+
+    plate: A matrix representing the plate of cell cultures.
+    """
+    n = len(plate[0])
+    print("\n\n" + n * "----")
+    for row in plate:
+        print('|', end="")
+        for well in row:
+            val = " X" if well == 1 else "  "
+            print(val, end="")
+            print(" |", end="")
+        print("\n" + n * "----")
+    print("\n\n")
+
+
+def save_plate(plate, row_range, col_range, save_dir):
+    """
+    This function creates an illustrative png of a well plate an marks
+    each infected well with a cross.
+    """
+    row_range = [chr(i)
+                 for i in range(ord(row_range[0]), ord(row_range[1]) + 1)]
+    col_range = list(range(col_range[0], col_range[1] + 1))
+
+    well_size = 100
+    well_border = 10
+
+    nb_col = len(plate[0])
+    nb_row = len(plate)
+
+    # Create a blank canvas
+    img = Image.new("RGBA",
+                    ((nb_col + 1) * well_size,
+                     (nb_row + 1) * well_size)
+                    )
+
+    font = ImageFont.truetype("assets/fonts/OpenSans-Regular.ttf", 80)
+    draw = ImageDraw.Draw(img)
+
+    for i in range(1, nb_row + 1):
+        # draw row letter
+        draw.text((0, i * well_size),
+                  row_range[i-1], fill="black", font=font)
+
+        for j in range(1, nb_col + 1):
+            # draw column number
+            draw.text((j * well_size + 2 * well_border, 0),
+                      str(col_range[j-1]), fill="black", font=font)
+
+            # define dimensions of well
+            dims = ((j * well_size + well_border,
+                     i * well_size + well_border),
+                    ((j + 1) * well_size - well_border,
+                     (i + 1) * well_size - well_border)
+                    )
+
+            # draw a well
+            draw.rectangle(dims, fill="white", outline="black", width=3)
+
+            # if infected, draw a cross
+            if plate[i-1][j-1]:
+                cross = (dims,
+                         ((j * well_size + well_border, (i + 1) * well_size - well_border),
+                          ((j + 1) * well_size - well_border, i * well_size + well_border))
+                         )
+
+                draw.line(cross[0], fill="black", width=3)
+                draw.line(cross[1], fill="black", width=3)
+
+    img.save(save_dir)
 
 
 def spear_karb(plate, d, d_0):
@@ -153,80 +263,26 @@ def find_x0(plate):
     return row0
 
 
-def get_plates(data_dir, col_range, row_range):
-    """
-    This function returns a matrix representing a plate of cell cultures
-    from a directory containing file paths to image files. 
-    It accepts three arguments:
-
-    data_dir: A string representing the path to the directory containing the image files.
-    col_range: A tuple representing the range of columns on the plate.
-    row_range: A tuple representing the range of rows on the plate.
-    """
-    filenames = os.listdir(data_dir)
-    plate = []
-
-    row_range = [chr(i)
-                 for i in range(ord(row_range[0]), ord(row_range[1]) + 1)]
-    col_range = list(range(col_range[0], col_range[1] + 1))
-
-    # loop through all coordinates of a plate
-    for row_nb in row_range:
-        row = []
-
-        for col_nb in col_range:
-            coord = row_nb + str(col_nb)
-
-            # find matches
-            matches = [file for file in filenames if coord in file]
-            file_path = os.path.join(data_dir, matches[0])
-            row.append(file_path)
-
-        plate.append(row)
-
-    return plate
-
-
-def display_plate(plate):
-    """
-    This function represents each infected well of a plate with an X.  It accepts
-    one argument:
-
-    plate: A matrix representing the plate of cell cultures.
-    """
-    n = len(plate[0])
-    print("\n\n" + n * "----")
-    for row in plate:
-        print('|', end="")
-        for well in row:
-            val = " X" if well == 1 else "  "
-            print(val, end="")
-            print(" |", end="")
-        print("\n" + n * "----")
-    print("\n\n")
-
+plate = [
+    [0, 0, 0, 0, 0, 0, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 0, 0],
+    [1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0]
+]
 
 # plate = [
-#     [0, 0, 0, 0, 0, 0, 0, 0],
-#     [1, 1, 1, 1, 1, 1, 1, 0],
-#     [1, 1, 1, 1, 1, 1, 0, 0],
-#     [1, 1, 0, 0, 0, 0, 0, 0],
-#     [0, 0, 0, 0, 0, 0, 0, 0]
+#     [1, 1, 1, 1],
+#     [1, 1, 1, 1],
+#     [1, 1, 1, 1],
+#     [1, 1, 1, 1],
+#     [1, 1, 1, 1],
+#     [1, 1, 1, 1],
+#     [1, 1, 1, 1],
+#     [1, 1, 1, 0],
+#     [1, 0, 0, 0],
+#     [0, 0, 0, 0],
 # ]
-
-
-plate = [
-    [1, 1, 1, 1],
-    [1, 1, 1, 1],
-    [1, 1, 1, 1],
-    [1, 1, 1, 1],
-    [1, 1, 1, 1],
-    [1, 1, 1, 1],
-    [1, 1, 1, 1],
-    [1, 1, 1, 0],
-    [1, 0, 0, 0],
-    [0, 0, 0, 0],
-]
 
 # # res = spear_karb(plate, 1, -1)
 
